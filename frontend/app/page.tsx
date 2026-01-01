200dff447779b5543974bf35ae92ac6a8dd7e26e
ARULSELVAN M <121343334+Arulselvan-65@users.noreply.github.com>
2026-01-01 19:47:58 +0530

    Uncomment promotional text and button in Home component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex    h-screen bg-gray-900 font-sans justify-center items-center">
-        {/*<p className="text-xl text-cyan-300 mb-8">Crowdfund projects. Earn exclusive NFT rewards.</p>*/}
-        {/*<Button size="lg">Create a Campaign</Button>*/}
+        <p className="text-xl text-cyan-300 mb-8">Crowdfund projects. Earn exclusive NFT rewards.</p>
+        <Button size="lg">Create a Campaign</Button>
         <Image src="/logo.svg" alt="logo" height={500} width={500}/>
 
     </div>
